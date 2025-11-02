ba93cb3eb2b642389e74c739150cc08a1f4f0159
snehapadgaonkar <padgaonkarsneha@gmail.com>
2025-11-03 03:29:23 +0530

    .

--- task_4/main.tf
+++ task_4/main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
 resource "docker_container" "nginx_container" {
   name  = var.container_name
   image = docker_image.nginx.image_id
